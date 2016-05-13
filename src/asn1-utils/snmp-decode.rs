b3cd8de1f4a6e50822cfd851addc25e9acb1cd9e
Josh Driver <keeperofdakeys@gmail.com>
2016-05-13 22:46:56 +0930

    Initial snmp decoding test

---
+++ src/asn1-utils/snmp-decode.rs
@@ -0,0 +1,93 @@
+extern crate asn1_cereal;
+extern crate argparse;
+extern crate serde;
+extern crate serde_json;
+
+use asn1_cereal::{tag, byte};
+use asn1_cereal::ber::stream;
+
+// SNMP ASN.1 Definition
+// https://tools.ietf.org/html/rfc1157#page-30
+
+
+struct Message {
+  version: i32,
+  community: String,
+  data: PDU,
+}
+
+// FIXME: These enum names and types are totally bogus.
+enum PDU {
+  get_request(PDU),
+  get_next_request(PDU),
+  get_response(PDU),
+  set_request(PDU),
+  trap(TrapPDU),
+}
+
+struct PDU {
+  request_id: i32,
+  error_status: i32,
+  error_index: i32,
+  variable_bindings: VarBindList,
+}
+
+struct TrapPDU {
+  enterprise: ObjectIdentifier,
+  agent_addr: NetworkAddress,
+  generic_trap: i32,
+  specific_trap: i32,
+  time_stamp: TimeTicks,
+  variable_bindings: VarBindList,
+}
+
+struct VarBind {
+  name: ObjectName,
+  value: ObjectSyntax,
+}
+
+type VarBindList = Vec<VarBind>;
+
+use std::io;
+use std::io::Read;
+use std::fs;
+use std::path::Path;
+use std::collections::BTreeMap;
+use argparse::{ArgumentParser, StoreTrue, StoreOption};
+use serde_json::value::Value;
+use serde_json::ser::to_string_pretty;
+
+fn main() {
+  let opts = parse_args();
+
+  let path = Path::new(opts.file.as_ref().unwrap());
+  if !path.is_file() {
+    panic!("Supplied file does not exist");
+  }
+
+  // Create a buffered reader from the file.
+  let reader = io::BufReader::new(fs::File::open(path).unwrap()).bytes();
+}
+
+struct ProgOpts {
+  file: Option<String>,
+  verbose: bool,
+}
+
+fn parse_args() -> ProgOpts {
+  let mut opts = ProgOpts {
+    file: None,
+    verbose: false,
+  };
+
+  {
+    let mut ap = ArgumentParser::new();
+    ap.set_description("Decode ASN.1 files");
+    ap.refer(&mut opts.verbose)
+      .add_option(&["-v", "--verbose"], StoreTrue, "Verbose output");
+    ap.refer(&mut opts.file)
+      .add_argument("file", StoreOption, "ASN.1 file to decode");
+    ap.parse_args_or_exit();
+  }
+  opts
+}
