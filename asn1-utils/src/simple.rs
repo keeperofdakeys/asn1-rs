8d9b442bbb604d99ae31a260b2328b2b9c261f65
Josh Driver <keeperofdakeys@gmail.com>
2016-09-07 22:16:49 +0930

    Add a --dump option to simple.rs

--- asn1-utils/src/simple.rs
+++ asn1-utils/src/simple.rs
@@ -1,20 +1,31 @@
 #[macro_use]
 extern crate asn1_cereal;
+extern crate argparse;
 
 use std::io;
 
 use asn1_cereal::serial::{Asn1Serialize, Asn1Deserialize};
-use asn1_cereal::enc::{DER, BER};
+use asn1_cereal::enc::{DER, BER, BERAlt};
 use asn1_cereal::tag::Class;
+use argparse::{ArgumentParser, StoreTrue};
 
 fn main() {
+  let opts = parse_args();
+
   let mut buffer: Vec<u8> = Vec::new();
-  // let mut buffer = io::BufWriter::new(io::stdout());
+  let mut output = io::BufWriter::new(io::stdout());
+
   let seq = IntSequence { a: 3, b: 4, c: SomeString("Hello".into()) };
+
+  if opts.dump {
+    let mut writer = io::BufWriter::new(&mut output);
+    seq.serialize_enc(BERAlt, &mut writer).unwrap();
+    return;
+  }
   {
     let mut writer = io::BufWriter::new(&mut buffer);
-    seq.serialize_enc(DER, &mut writer).unwrap();
-  };
+    seq.serialize_enc(BERAlt, &mut writer).unwrap();
+  }
   println!("{:?}", buffer);
   {
     let mut reader = buffer.iter().map(|x| Ok(*x) as Result<u8, std::io::Error>);
@@ -50,3 +61,20 @@ asn1_sequence!(
   b,
   c
 );
+
+struct ProgOpts {
+  dump: bool,
+}
+
+fn parse_args() -> ProgOpts {
+  let mut opts = ProgOpts {
+    dump: false,
+  };
+  {
+    let mut ap = ArgumentParser::new();
+    ap.refer(&mut opts.dump)
+      .add_option(&["-d", "--dump"], StoreTrue, "Dump asn.1 packet");
+    ap.parse_args_or_exit();
+  }
+  opts
+}
