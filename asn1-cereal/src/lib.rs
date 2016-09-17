c0c1ace979e537a10969017cded6bd6bc3a0fe52
Josh Driver <keeperofdakeys@gmail.com>
2016-09-18 01:27:31 +0930

    Add example to root lib.rs

--- asn1-cereal/src/lib.rs
+++ asn1-cereal/src/lib.rs
@@ -6,6 +6,28 @@
 //!
 //! The `Asn1Info`, `BerSerialize` and `BerDeserialize` traits are what
 //! most users will want to use.
+//!
+//! ```
+//! #[macro_use] extern crate asn1_cereal; fn main() {
+//!   struct ShortSequence {
+//!     z: u64,
+//!     y: u32,
+//!   }
+//!
+//!   ber_sequence!(
+//!     ShortSequence,
+//!     "SHORT_SEQUENCE",
+//!     z,
+//!     y
+//!   );
+//!
+//!   use asn1_cereal::BerSerialize;
+//!
+//!   let data = ShortSequence{ z: 1, y: 2 };
+//!   let mut bytes: Vec<u8> = Vec::new();
+//!   BerSerialize::serialize(&data, &mut bytes).unwrap();
+//! }
+//! ```
 
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
