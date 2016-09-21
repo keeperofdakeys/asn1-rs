74396be90f4d2ead1edf7b715769fc58aabde35c
Josh Driver <keeperofdakeys@gmail.com>
2016-09-21 22:31:59 +0930

    Add a next_token parser

--- asn1-nom/src/lib.rs
+++ asn1-nom/src/lib.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate nom;
 
+use nom::{space, is_alphanumeric};
+
 // LDAPString ::= OCTET STRING -- UTF-8 encoded,
 //                             -- [ISO10646] characters
 
@@ -27,10 +29,11 @@ named!(type_name <&[u8], String>, chain!(
 ));
 
 named!(type_assignment <&[u8], Asn1Def>, chain!(
-  opt!(nom::multispace) ~
+  next_token? ~
   name: type_name ~
-  take_until_and_consume!("::=") ~
-  opt!(nom::multispace) ~
+  next_token ~
+  tag!("::=") ~
+  next_token ~
   assign: type_name,
   || Asn1Def {
     name: name,
@@ -45,6 +48,39 @@ named!(type_sequence <&[u8], Asn1Seq>, chain!(
   }
 ));
 
+named!(strip_comments <String>, chain!(
+  test: take_until!("--"),
+  || String::from_utf8(test.to_owned()).unwrap()
+));
+
+named!(strip_comments_2, take_until!("--"));
+
+pub fn is_eol(byte: &u8) -> bool {
+  let chr = *byte as char;
+  println!("{}", byte);
+  chr == '\n' || chr == '\r'
+}
+
+named!(next_token <()>, chain!(
+  space? ~
+  chain!(
+    tag!("--") ~
+    take_till!(is_eol) ~
+    tag!("\n") ~
+    space?,
+    || ()
+  )? ,
+  || ()
+));
+
+named!(test_ten, chain!(
+  next_token ~
+  next_token ~
+  s: take_while!(nom::is_alphanumeric),
+  || s
+));
+
 fn main() {
-  println!("{:#?}", type_assignment("test ::= hi".as_bytes()));
+  println!("{:#?}", type_assignment("test -- ::=\n::= hi".as_bytes()));
+  println!("{:#?}", String::from_utf8(test_ten("  --fds\ndlsjfs\nfds ::= hi".as_bytes()).unwrap().0.to_owned()).unwrap());
 }
