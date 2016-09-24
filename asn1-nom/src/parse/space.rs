854046c11eb5c64a3777e71834556614bcc6844d
Josh Driver <keeperofdakeys@gmail.com>
2016-09-24 23:53:42 +0930

    Add more whitespace testing

--- asn1-nom/src/parse/space.rs
+++ asn1-nom/src/parse/space.rs
@@ -1,4 +1,4 @@
-use nom::{multispace, eol, newline};
+use nom::{multispace, eol};
 
 named!(pub comment <()>, chain!(
   tag!("--") ~
@@ -26,9 +26,48 @@ pub fn is_eol(byte: u8) -> bool {
 
 #[test]
 fn test_comment() {
-  println!("{:?}", comment("--\n".as_bytes()));
   assert_eq!(
     "".as_bytes(),
     comment("--\n".as_bytes()).unwrap().0
   );
+  assert_eq!(
+    "".as_bytes(),
+    comment("--foo bar asdf\n".as_bytes()).unwrap().0
+  );
+  assert_eq!(
+    "  ".as_bytes(),
+    comment("--\n  ".as_bytes()).unwrap().0
+  );
+  assert_eq!(
+    "foobar".as_bytes(),
+    comment("--asdf\nfoobar".as_bytes()).unwrap().0
+  );
+  assert!(comment("foo --\n".as_bytes()).is_err());
+  assert!(comment(" bar --\n".as_bytes()).is_err());
+  assert!(comment(" -asdf-\n".as_bytes()).is_err());
+  assert!(comment("-".as_bytes()).is_incomplete());
+  assert!(comment("--".as_bytes()).is_incomplete());
+}
+
+#[test]
+fn test_skip_other() {
+  assert_eq!(
+    "".as_bytes(),
+    skip_other("  --\n".as_bytes()).unwrap().0
+  );
+  assert_eq!(
+    "".as_bytes(),
+    skip_other("--\n  ".as_bytes()).unwrap().0
+  );
+  assert_eq!(
+    "".as_bytes(),
+    skip_other("--\n \n ".as_bytes()).unwrap().0
+  );
+  assert_eq!(
+    "".as_bytes(),
+    skip_other("--\n -- adf\n ".as_bytes()).unwrap().0
+  );
+  assert!(skip_other(" -".as_bytes()).is_incomplete());
+  assert!(skip_other(" --".as_bytes()).is_incomplete());
+  assert!(skip_other(" --\n -".as_bytes()).is_done());
 }
